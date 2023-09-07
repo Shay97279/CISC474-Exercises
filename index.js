ff76524265535c45235fc92ad1a63fedb208aa58
patelsap <patelsap@udel.edu>
2023-09-07 09:16:00 -0400

    got exercise 3 working with index.js

--- index.js
+++ index.js
@@ -4,17 +4,12 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
 
-    $("#names").on("submit", function () {
 
-        let firstname = $('#fname').val();
+    });
 
-        let lastname = $('#lname').val();
-
-
-        $("#jumbotron").text(`${firstname} ${lastname}`);
-
+$(document).on("submit", function () {
+    let firstname = $('#fname').val();
+    let lastname = $('#lname').val();
+    $("#jumbotron").text(firstname + " " + lastname);
         return false;
-
-    })
-
     });
