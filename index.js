918b4c3476d678b127f31f0a7c2f26c07818dbfc
patelsap <patelsap@udel.edu>
2023-09-07 08:55:33 -0400

    added js, modified  with input id

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 $( document ).ready(function() {
+
+
     console.log( "ready!" );
+
+
+    $("#names").on("submit", function () {
+
+        let firstname = $('#fname').val();
+
+        let lastname = $('#lname').val();
+
+
+        $("#jumbotron").text(`${firstname} ${lastname}`);
+
+        return false;
+
+    })
+
     });
